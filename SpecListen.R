2eb8ad5d713d3eb8f10e539e9abe28520b012084
Patrick Lorch <plorch@southernsierraresearch.org>
2024-08-07 12:50:35 -0700

    tweaks to improve verifications

--- SpecListen.R
+++ SpecListen.R
@@ -1,15 +1,22 @@
 ## Function that creates a spectrogram and plays a the sound for one 
 ## row of a selection table
-
-spec_listen = function(df = sel_table, sel_row = sel_file_row)
+library(tuneR)
+library(seewave)
+spec_listen = function(df = sel_table, sel_row = sel_file_row, 
+                       buf = 0.25, freqlim = c(0, 10))
 {
+  # Debug code, uncomment
+  # print(df[sel_row,])
   focal_row = df[sel_row,]
   sel_wav = readWave(focal_row$filepath,
                      from = focal_row$start - buf,
                      to = focal_row$end + buf,
                      units = "seconds")
   spectro(sel_wav, 
-          main = paste(focal_row$common_name, focal_row$confidence), 
+          main = paste(focal_row$common_name, focal_row$confidence),
+          palette = reverse.gray.colors.2,
+          noisereduction = 1,
+          flim = freqlim,
           listen = T)
 }
 
